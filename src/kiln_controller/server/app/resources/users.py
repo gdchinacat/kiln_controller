0b3d8fee55ff595e94b463cf96beefaa248a6e74
Lonnie Hutchinson <chinacat@chinacat.org>
2025-08-14 21:07:42 -0700

    make the implementation generic for easy reuse with other types

--- src/kiln_controller/server/app/resources/users.py
+++ src/kiln_controller/server/app/resources/users.py
@@ -13,57 +13,67 @@ def db(func):
 def error(msg):
     return {"error": msg}
 
-class UserResource(Resource):
+class BaseResource(Resource):
+    TYPE = None
     
     def _lookup(self, db, pk):
         try:
-            return db.session.execute(db.select(User).filter_by(id=pk)).scalar_one()
+            return db.session.execute(db.select(self.TYPE).filter_by(id=pk)).scalar_one()
         except NoResultFound:
             return None
+
     @db
     def get(self, pk, *, db):
-        user = self._lookup(db, pk)
-        if not user:
-            return error(f"user with id={pk} not found"), HTTPStatus.NOT_FOUND
-        return user.asdict()
+        orm = self._lookup(db, pk)
+        if not orm:
+            return error(f"{self.TYPE.__name__} with id={pk} not found"), HTTPStatus.NOT_FOUND
+        return orm.asdict()
     
     @db
     def put(self, pk, *, db):
         j = request.json
-        user = self._lookup(db, pk)
-        if user is None:
+        orm = self._lookup(db, pk)
+        if orm is None:
             if 'name' not in j:
                 return error("'name' is required"),  HTTPStatus.UNPROCESSABLE_ENTITY
-            user = User(j['name'])
-            user.id = pk
-            db.session.add(user)
-        for attr in user.asdict().keys():#('name', 'email', 'phone_number'):
+            orm = self.TYPE(j['name'])
+            orm.id = pk
+            db.session.add(orm)
+        for attr in orm.asdict().keys():#('name', 'email', 'phone_number'):
             if attr in j:
-                setattr(user,attr, j[attr])
+                setattr(orm, attr, j[attr])
                 del j[attr]
         #raise an error if any attributes can't be processed.
         if j:
             return error(f"unexpected values: {j}"), HTTPStatus.UNPROCESSABLE_ENTITY
         
         current_app.db.session.commit()
-        return user.asdict()
+        return orm.asdict()
     
     @db
     def delete(self, pk, *, db):
-        user = self._lookup(db, pk)
-        if user is not None:
-            db.session.delete(user)
+        orm = self._lookup(db, pk)
+        if orm is not None:
+            db.session.delete(orm)
             db.session.commit()
         return {}
     
-class UserListResource(Resource):
+class ListResource(Resource):
+    TYPE = None
+    
     @db
     def get(self,*, db):
-        users = db.session.execute(db.select(User)).scalars()
-        return [user.asdict() for user in users]
+        orms = db.session.execute(db.select(self.TYPE)).scalars()
+        return [orm.asdict() for orm in orms]
     
     def post(self):
-        user = User(**request.json)
-        current_app.db.session.add(user)
+        orm = self.TYPE(**request.json)
+        current_app.db.session.add(orm)
         current_app.db.session.commit()
-        return user.asdict()
+        return orm.asdict()
+    
+class UserResource(BaseResource):
+    TYPE = User
+    
+class UserListResource(ListResource):
+    TYPE = User
